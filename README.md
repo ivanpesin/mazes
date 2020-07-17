01ac4b3c50517ec9834f9092248cad8183a69b9f
Ivan Pesin <ivan.pesin@gmail.com>
2020-07-18 00:16:07 +0300

    changed CLI opt -b to -a, added --algs to list supported algs

--- README.md
+++ README.md
@@ -2,18 +2,40 @@
 
 Python implementations for maze generation, visualization, and solving.
 
-## Example
+## Syntax
+
+```
+usage: maze_client.py [-h] [-a A] [--algs] [-d D] [-n N] [-w W]
+
+Creates an NxN maze using the specified algorithm.
+
+optional arguments:
+  -h, --help  show this help message and exit
+  -a A        maze generation algorithm
+  --algs      list supported maze generation algorithms
+  -d D        simulation delay in seconds, can be a fraction
+  -n N        maze dimention
+  -w W        tile width
+```
+
+## Examples
 
 ### Recursive split
 
 Recursive splitting 50/50 and opening a randomly positioned door:
 
+`$ python3 maze_client.py -n 10 -w 30 -d 0.2`
+
 ![](images/maze-split-halves.gif)
 
 Recursive splitting at a random point and opening a randomly positioned door:
 
+`$ python3 maze_client.py -n 10 -w 30 -d 0.2 -a 2`
+
 ![](images/maze-split-random.gif)
 
 ### Recursive backtracking
+
+`$ python3 maze_client.py -n 10 -w 30 -d 0.2 -a 2`
 
 ![](images/maze-recursive-bt.gif)
--- maze_client.py
+++ maze_client.py
@@ -6,19 +6,37 @@ import maze_solver
 import maze_visualizer
 import tkinter
 import time
-import random
 import argparse
+import random
+import sys
 
+ALGS_LIST = [
+    'Recursive splitting 50/50',
+    'Recursive splitting at random',
+    'Recursive backtracking'
+]
+
+def show_algs():
+    print("List of supported maze generation algorithms:\n")
+    for i, name in enumerate(ALGS_LIST):
+        print("\t%d\t%s" % (i, name))
+
+    
 # init random generator
 random.seed()
 
 # parse the command line
-parser = argparse.ArgumentParser(description='Creates an NxN maze using recursive splitting algorithm.')
-parser.add_argument('-b', default=0, type=int,   help="maze building algorithm")
+parser = argparse.ArgumentParser(description='Creates an NxN maze using the specified algorithm.')
+parser.add_argument('-a', default=0, type=int,   help="maze generation algorithm")
+parser.add_argument('--algs', action="store_true", help="list supported maze generation algorithms")
 parser.add_argument('-d', default=0, type=float, help="simulation delay in seconds, can be a fraction")
 parser.add_argument('-n', default=10, type=int,  help="maze dimention")
 parser.add_argument('-w', default=10, type=int,  help="tile width")
 args = parser.parse_args()
+
+if args.algs:
+    show_algs()
+    sys.exit()
 
 # create tkinter root
 tk = tkinter.Tk()
@@ -27,7 +45,7 @@ tk.geometry('+100+100')
 
 # create a maze; algs 0-4 start with empty maze and build walls, 
 # others carve doors in a maze full of walls
-if args.b in [0,1]: m = maze.Maze(N=args.n, walls=False)
+if args.a in [0,1]: m = maze.Maze(N=args.n, walls=False)
 else: m = maze.Maze(N=args.n)
 
 # create visualizator to use with generation and solving of the maze
@@ -36,13 +54,13 @@ vis.draw_tk_maze()
 time.sleep(3)
 
 # ---
-if   args.b == 0: 
+if   args.a == 0: 
     vis.set_statusbar('Generating the maze: recursive half-split')
     maze_generator.RecursiveSplit(m,vis)
-elif args.b == 1: 
+elif args.a == 1: 
     vis.set_statusbar('Generating the maze: recursive random split')
     maze_generator.RecursiveSplit(m,vis,mode='random')
-elif args.b == 2: 
+elif args.a == 2: 
     vis.set_statusbar('Generating the maze: recursive backtracking')
     maze_generator.RecursiveBacktracking(m,vis)
 
